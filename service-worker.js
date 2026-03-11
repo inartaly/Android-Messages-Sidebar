c06fa5b6aeb6c00cfca5c969418f781cb816402d
inartaly <inartaly@gmail.com>
2026-03-11 10:24:51 -0400

    Main

--- service-worker.js
+++ service-worker.js
@@ -5,9 +5,9 @@ chrome.sidePanel
 chrome.runtime.onInstalled.addListener(() => {
   // This tells the extension: "When you open the sidebar, go straight to Google"
   chrome.sidePanel.setOptions({
-    path: 'https://messages.google.com/web/authentication',
-    enabled: true
+    path: "https://inartaly.github.io/Browser-Extension-Icon-Resizer/",
+    enabled: true,
   });
   // This opens the side panel immediately after installation
-  console.log("Extension installed. Ready to message!");
+  console.log("Extension installed. Ready to resize icons!");
 });
